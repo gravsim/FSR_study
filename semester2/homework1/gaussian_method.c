64c580ba0e04a6dc41a2e48b17c19bffc0fdd8c8
gravsim <ilyaklopov07@yandex.ru>
2026-02-27 15:16:35 -0500

    #00000003

---
+++ semester2/homework1/gaussian_method.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int Gaussian_method(int matrix_size, int**matrix) {
+    
+
+}
+
+
+int main(void) {
+    int matrix_size;
+    int i;
+    scanf("%d", &matrix_size);
+    int** matrix = calloc(matrix_size, sizeof(int*));
+    for (i = 0; i < matrix_size; i++) {
+        scanf("%d", matrix[i] + j);
+    }
+    Gaussian_method(matrix_size, matrix);
+    free(matrix);
+    return 0;
+}
